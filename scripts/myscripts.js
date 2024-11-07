f46b2200d2e7a4a724187afb51697e0449770b97
alexiskrueger5752 <165833941+alexiskrueger5752@users.noreply.github.com>
2024-11-07 01:48:55 -0800

    Add files via upload

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,26 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const galleryWrapper = document.querySelector('.gallery-wrapper');
-  
-  if (galleryWrapper) {
-      for (let i = 1; i <= 10; i++) {
-          const img = document.createElement('img');
-          img.src = `images/rainier${i}.jpg`;  // 
-          img.alt = `Rainier Image ${i} by Lily M. Tang`;
-          img.style.width = "100%";
-          img.style.height = "auto";
-          galleryWrapper.appendChild(img);
-          
-      }
-  } else {
-      console.error("Gallery wrapper not found.");
-  }
-// scroll every 3 seconds
-let currentIndex = 0;
-    const totalImages = 10;
-    
-    function scrollGallery() {
-        currentIndex = (currentIndex + 1) % totalImages;
-        galleryWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
-    setInterval(scrollGallery, 3000);
-});
+
+function showAlt(x) {
+  document.getElementById("alttext").innerHTML=x.alt;
+  // document.getElementById("alttext").style.border = "1px solid black";
+  // document.getElementsById("index-alttext").style.background = "rgba(16, 185, 30, 0.911)";
+}
+
+function hideAlt(x) {
+  document.getElementById("alttext").innerHTML="";
+  // document.getElementById("alttext").style.border="0px";
+}
